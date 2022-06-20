f19da9fb231fe66a0772361afbdbcc0b037314e1
Мегрелишвили Георгий М <gm.megrelishvili@jet.su>
2022-06-20 16:41:05 +0300

    ./

---
+++ pythonProject/main2.py
@@ -0,0 +1,9 @@
+# This is a sample Python script.
+
+# Press Shift+F10 to execute it or replace it with your code.
+# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
+
+
+for i in range(0,30):
+    print ("Number is ", end="->")
+    print(i)
